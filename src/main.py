ad708df1fc553f18af548796ac932d30c73db5a1
perryhowe <perry.howe@gmail.com>
2026-05-10 18:45:37 +0100

    Initial commit

---
+++ src/main.py
@@ -0,0 +1,8 @@
+from textnode import TextNode
+
+def main():
+    textnode = TextNode('This is some anchor text', 'bold', 'https://github.com/perryhowe')
+    print(textnode)
+
+if __name__ == "__main__":
+    main()
