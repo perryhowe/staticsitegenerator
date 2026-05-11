752799d1ef193e23de981a9625cd26dd3969f10b
perryhowe <perry.howe@gmail.com>
2026-05-11 09:39:15 +0100

    feat: add leafnode

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,5 @@
 import unittest
-
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -26,3 +25,36 @@ class TestHTMLNode(unittest.TestCase):
     def test_repr(self):
         node = HTMLNode("p", "hello", None, {"class": "text"})
         self.assertEqual("HTMLNode(p, hello, None, {'class': 'text'})", repr(node))
+
+
+class TestLeafNode(unittest.TestCase):
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leaf_to_html_with_props(self):
+        node = LeafNode("a", "google.com", {"href": "https://google.com"})
+        self.assertEqual(
+            node.to_html(),
+            '<a href="https://google.com">google.com</a>',
+        )
+
+    def test_leaf_to_html_no_value(self):
+        node = LeafNode("p", None)
+        with self.assertRaises(ValueError):
+            node.to_html()
+
+    def test_leaf_to_html_no_tag(self):
+        node = LeafNode(None, "raw text")
+        self.assertEqual(node.to_html(), "raw text")
+
+    def test_leaf_repr(self):
+        node = LeafNode("p", "hello", {"class": "text"})
+        self.assertEqual(
+            node.__repr__(),
+            "LeafNode(p, hello, {'class': 'text'})",
+    )
+
+
+if __name__ == "__main__":
+    unittest.main()
--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,7 +1,5 @@
 import unittest
-
 from textnode import TextNode, TextType
-
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
@@ -28,7 +26,6 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is a text node", TextType.BOLD, url=None)
         node2 = TextNode("This is a text node", TextType.BOLD, url="https://github.com/perryhowe")
         self.assertNotEqual(node, node2)
-
 
 if __name__ == "__main__":
     unittest.main()
